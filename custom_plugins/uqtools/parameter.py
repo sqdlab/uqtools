637c4ba921e31bd59d7d41893af3f277be32ea38
Markus Jerger <devel@joe1gi.de>
2015-01-08 01:39:11 +0000

    [uqtools] uqtools.parameter tests and reformatting, removed **kwargs from get

--- custom_plugins/uqtools/parameter.py
+++ custom_plugins/uqtools/parameter.py
@@ -2,26 +2,30 @@ import numpy
 
 class Parameter(object):
     '''
-        a parameter of a data object
-        
-        by making parameter objects it becomes possible to store them inside nested measurement
-        functions/objects. when a nested measurement is run, it can retrieve the current point
-        on the coordinate axes by accessing its stored parameter objects.
-        
-        additional properties like the parameter name can be automatically added to new data files
-        created by nested measurements. 
+    a parameter of a data object
+    
+    by making parameter objects it becomes possible to store them inside 
+    nested measurement functions/objects. when a nested measurement is run, 
+    it can retrieve the current point on the coordinate axes by accessing 
+    its stored parameter objects.
+    
+    additional properties like the parameter name can be automatically 
+    added to new data files created by nested measurements. 
     '''
-    def __init__(self, name, set_func=None, get_func=None, value=None, dtype=None, **options):
+    def __init__(self, name, set_func=None, get_func=None, value=None, 
+                 dtype=None, **options):
         '''
-            initialize parameter.
-            
-            Input:
-                name - frieldy name of the parameter
-                set_func - function called when set(value) is called
-                get_func - function called when get() is called, returns stored value if not given
-                value - stored value. not necessarily scalar.
-                dtype - data type
-                **info - extra keyword arguments are descriptive information and may be stored in data files
+        initialize parameter.
+        
+        Input:
+            name - friendly name of the parameter
+            set_func - function called when set(value) is called
+            get_func - function called when get() is called, returns stored 
+                value if not given
+            value - stored value. not necessarily scalar.
+            dtype - data type
+            **options - extra keyword arguments are descriptive information 
+                and may be stored in data files
         '''
         self.name = name
         self.set_func = set_func
@@ -34,27 +38,34 @@ class Parameter(object):
         self.options = options
     
     def set(self, value):
-        ''' store value and call set_func if defined '''
+        ''' 
+        store value AND call set_func if defined 
+        '''
         if(self.set_func):
             self.set_func(value)
         self._value = value
     
-    def get(self,  **kwargs):
-        ''' return result of get_func or stored value if no get_func was defined '''
+    def get(self):
+        '''
+        return result of get_func OR stored value if no get_func was defined 
+        '''
         if(self.get_func):
             return self.get_func()
         return self._value
     
     def iscomplex(self):
-        ''' check if self.dtype is a complex type '''
+        ''' 
+        check if self.dtype is a complex type
+        '''
         return callable(self.dtype) and numpy.iscomplexobj(self.dtype())
         
     def __repr__(self):
-        r = super(Parameter, self).__repr__()
+        ''' return a human-readable representation of self '''
+        super_repr = super(Parameter, self).__repr__()
         # <object Parameter "name" at 0x...>
-        r_parts = r.split(' ')
-        r_parts.insert(2, '"{0}"'.format(self.name))
-        return ' '.join(r_parts)
+        repr_parts = super_repr.split(' ')
+        repr_parts.insert(2, '"{0}"'.format(self.name))
+        return ' '.join(repr_parts)
     
     
 class ParameterList(list):
@@ -62,6 +73,7 @@ class ParameterList(list):
     A list of parameters that implements element access by name and index.
     '''
     def __getitem__(self, key):
+        ''' element access by integer index or parameter.name '''
         if isinstance(key, int):
             return list.__getitem__(self, key)
         for item in self:
---
+++ custom_plugins/uqtools/test_parameter.py
@@ -0,0 +1,69 @@
+import pytest
+import numpy
+
+from parameter import Parameter, ParameterList
+
+def test_as_buffer():
+    p = Parameter('test')
+    p.set(-1)
+    assert p.get() == -1
+
+class TestParameter:
+    def test_as_buffer(self):
+        p = Parameter('test')
+        p.set(-1)
+        assert p.get() == -1
+        
+    def test_init_value(self):
+        p = Parameter('test', value=-1)
+        assert p.get() == -1
+        
+    def test_get_func(self):
+        p = Parameter('test', get_func=lambda: -1)
+        assert p.get() == -1
+        
+    def test_set_func(self):
+        self.set_val = None
+        def set_func(val):
+            self.set_val = val
+        p = Parameter('test', set_func=set_func)
+        p.set(-1)
+        assert self.set_val == -1
+
+    def test_iscomplex1(self):
+        p = Parameter('test')
+        assert not p.iscomplex()
+        
+    def test_iscomplex2(self):
+        p = Parameter('test', dtype=float)
+        assert not p.iscomplex()
+    
+    def test_iscomplex3(self):
+        p = Parameter('test', dtype=complex)
+        assert p.iscomplex()
+        
+    def test_iscomplex4(self):
+        p = Parameter('test', dtype=numpy.complex128)
+        assert p.iscomplex()
+        
+    def test_options(self):
+        options = {'o0': 0, 'o1': 1}
+        p = Parameter('test', **options)
+        assert p.options == options
+        
+    def test_repr(self):
+        # make sure __repr__ does not raise an exception
+        p = Parameter('test')
+        assert repr(p)
+        
+class TestParameterList:
+    @pytest.fixture
+    def populated_list(self):
+        self.ps = [Parameter('test{0}'.format(idx)) for idx in range(10)] 
+        return ParameterList(self.ps)
+    
+    def test_index_access(self, populated_list):
+        assert self.ps[0] == populated_list[0]
+        
+    def test_name_access(self, populated_list):
+        assert self.ps[0] == populated_list[self.ps[0].name]
