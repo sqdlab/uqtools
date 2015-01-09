e9d30fb94383c6892dd8f8a371c09db96f19c960
Markus Jerger <devel@joe1gi.de>
2015-01-09 09:18:35 +0000

    [uqtools] ParameterList -> TypeList, ParameterList subclass

--- custom_plugins/uqtools/parameter.py
+++ custom_plugins/uqtools/parameter.py
@@ -1,4 +1,4 @@
-import numpy
+from collections import OrderedDict, MutableSequence
 
 class Parameter(object):
     '''
@@ -53,12 +53,6 @@ class Parameter(object):
             return self.get_func()
         return self._value
     
-    def iscomplex(self):
-        ''' 
-        check if self.dtype is a complex type
-        '''
-        return callable(self.dtype) and numpy.iscomplexobj(self.dtype())
-        
     def __repr__(self):
         ''' return a human-readable representation of self '''
         super_repr = super(Parameter, self).__repr__()
@@ -66,17 +60,140 @@ class Parameter(object):
         repr_parts = super_repr.split(' ')
         repr_parts.insert(2, '"{0}"'.format(self.name))
         return ' '.join(repr_parts)
+
+    @staticmethod
+    def is_compatible(obj):
+        ''' 
+        test if an object supports all properties expected from a Parameter 
+        '''
+        return (hasattr(obj, 'name') and
+                hasattr(obj, 'get') and callable(obj.get) and 
+                hasattr(obj, 'set') and callable(obj.set) and
+                hasattr(obj, 'dtype'))
     
+
     
-class ParameterList(list):
+class TypedList(MutableSequence):
     '''
-    A list of parameters that implements element access by name and index.
+    A list that contains only elements compatible with a signature defined by 
+    a test function. Taylored towards Parameter and Measurement, a .name 
+    property is expected to be present for all elements.
+    '''
+
+    def __init__(self, is_compatible_func, iterable=()):
+        '''
+        Create a new typed list.
+        
+        Input:
+            is_compatible_func: function called on each added object to test
+                if its type is compatible with the expected item type.
+            iterable: initial list contents
+        '''
+        super(TypedList, self).__init__()
+        self.is_compatible_item = is_compatible_func
+        self.data = list()
+        self.extend(iterable)
+
+
+    def _check_compatible(self, obj):
+        ''' raise TypeError if obj is not of a compatible type. '''
+        if not self.is_compatible_item(obj):
+            raise TypeError('obj is of an incompatible type.')
+        
+    def __getitem__(self, idx):
+        ''' element access by integer index or obj.name '''
+        if isinstance(idx, int):
+            return self.data.__getitem__(idx)
+        for item in self.data:
+            if item.name == idx:
+                return item
+        raise IndexError(idx)
+    
+    def __setitem__(self, idx, obj):
+        ''' set item at idx. '''
+        self._check_compatible(obj)
+        self.data.__setitem__(idx, obj)
+        
+    def __delitem__(self, idx):
+        ''' remove item at idx. '''
+        self.data.__delitem__(idx)
+    
+    def __len__(self):
+        ''' return number of items. '''
+        return self.data.__len__()
+
+    def insert(self, idx, obj):
+        ''' insert obj before idx. '''
+        self._check_compatible(obj)
+        self.data.insert(idx, obj)
+
+    def index(self, obj):
+        '''
+        Return first index of obj. If not found, try item.name for all items.
+        Raises ValueError if the obj is not found.
+        '''
+        try:
+            return self.data.index(obj)
+        except ValueError as err:
+            for idx, item in enumerate(self.data):
+                if item.name == obj:
+                    return idx
+            raise err
+        
+    def __contains__(self, obj):
+        ''' test if obj in self or obj==item.name for any item. '''
+        try:
+            self.index(obj)
+        except ValueError:
+            return False
+        return True
+
+
+
+class ParameterList(TypedList):
+    
+    def __init__(self, iterable=()):
+        is_compatible_func = Parameter.is_compatible
+        super(ParameterList, self).__init__(is_compatible_func, iterable)
+        
+    def values(self):
+        '''
+        Return the result of Parameter.get() called on all contained objects.
+        '''
+        return [parameter.get() for parameter in self.data]
+
+
+
+class ParameterDict(OrderedDict):
+    '''
+    An OrderedDict that accepts string keys as well as Parameter keys 
+    for read access.
     '''
     def __getitem__(self, key):
-        ''' element access by integer index or parameter.name '''
-        if isinstance(key, int):
-            return list.__getitem__(self, key)
-        for item in self:
-            if item.name == key:
-                return item
-        raise IndexError(key)
+        '''
+        Retrieve an element by key.
+        
+        key can be a Parameter object or a string that is matched against
+        Parameter.name of all keys of the dictionary.  
+        '''
+        try:
+            # try key directly
+            return OrderedDict.__getitem__(self, key)
+        except KeyError as err:
+            # compare key to .name property of items
+            #if hasattr(key, 'name'): 
+            #    key = key.name
+            for parameter in self.keys():
+                if parameter.name == key:
+                    return OrderedDict.__getitem__(self, parameter)
+            raise err
+
+    def keys(self):
+        '''
+        Return a ParameterList of the keys of self.
+        '''
+        return ParameterList(super(ParameterDict, self).keys())
+
+    def __repr__(self):
+        items = ['"{0}":{1}'.format(k.name, v) for k, v in self.iteritems()]
+        return 'ParameterDict(' + ', '.join(items) + ')'
--- custom_plugins/uqtools/test_parameter.py
+++ custom_plugins/uqtools/test_parameter.py
@@ -1,7 +1,7 @@
-import pytest
+from pytest import fixture, raises
 import numpy
 
-from parameter import Parameter, ParameterList
+from parameter import Parameter, ParameterList, ParameterDict
 
 class TestParameter:
     def test_as_buffer(self):
@@ -25,34 +25,105 @@ class TestParameter:
         p.set(-1)
         assert self.set_val == -1
 
-    def test_iscomplex(self):
-        p = Parameter('test')
-        assert not p.iscomplex()
-        p = Parameter('test', dtype=float)
-        assert not p.iscomplex()
-        p = Parameter('test', dtype=complex)
-        assert p.iscomplex()
-        p = Parameter('test', dtype=numpy.complex128)
-        assert p.iscomplex()
-        
     def test_options(self):
         options = {'o0': 0, 'o1': 1}
         p = Parameter('test', **options)
         assert p.options == options
         
+    def test_is_compatible(self):
+        p = Parameter('test')
+        assert Parameter.is_compatible(p)
+        assert not Parameter.is_compatible(ParameterList())
+        assert not Parameter.is_compatible(ParameterDict())
+
     def test_repr(self):
         # make sure __repr__ does not raise an exception
         p = Parameter('test')
         assert repr(p)
-        
+
+
+
 class TestParameterList:
-    @pytest.fixture
-    def populated_list(self):
-        self.ps = [Parameter('test{0}'.format(idx)) for idx in range(10)] 
+    @fixture
+    def def_list(self):
+        self.ps = [Parameter('test{0}'.format(idx), value=idx) 
+                   for idx in range(10)]
+        self.pX = Parameter('invalid') 
         return ParameterList(self.ps)
     
-    def test_index_access(self, populated_list):
-        assert self.ps[0] == populated_list[0]
+    def test_getitem_index(self, def_list):
+        assert self.ps[0] == def_list[0]
         
-    def test_name_access(self, populated_list):
-        assert self.ps[0] == populated_list[self.ps[0].name]
+    def test_getitem_name(self, def_list):
+        assert self.ps[0] == def_list[self.ps[0].name]
+
+    def test_index(self, def_list):
+        assert def_list.index(self.ps[1]) == 1
+        assert def_list.index(self.ps[1].name) == 1
+        with raises(ValueError):
+            def_list.index(self.pX)
+        with raises(ValueError):
+            def_list.index(self.pX.name)
+
+    def test_values(self, def_list):
+        assert def_list.values() == range(len(self.ps))
+    
+    def test_append(self):
+        pl = ParameterList()
+        pl.append(Parameter('test1'))
+        with raises(TypeError):
+            pl.append(None)
+        with raises(TypeError):
+            pl.append([Parameter('test2')])
+    
+    def test_extend(self):
+        pl = ParameterList()
+        pl.extend([Parameter('test1'), Parameter('test2')])
+        with raises(TypeError):
+            pl.extend([Parameter('test3'), list()])
+    
+    def test_setitem(self, def_list):
+        with raises(TypeError):
+            def_list[0] = None
+        def_list[0] = Parameter('test2')
+        
+    def test_insert(self, def_list):
+        def_list.insert(0, Parameter('testN'))
+        with raises(TypeError):
+            def_list.insert(0, None)
+
+
+        
+class TestParametertDict:
+    @fixture
+    def keys_and_dict(self):
+        p0 = Parameter('p0')
+        p1 = Parameter('p1')
+        keys = [p0, p1]
+        return keys, ParameterDict( zip(keys, range(len(keys))) )
+
+    def test_getitem_name(self, keys_and_dict):
+        _, rd = keys_and_dict
+        assert rd['p1'] == 1
+        with raises(KeyError):
+            rd['pX']
+            
+    def test_getitem_parameter(self, keys_and_dict):
+        keys, rd = keys_and_dict
+        pX = Parameter('pX')
+        p1 = Parameter(keys[0].name) # object different from keys[0]
+        assert rd[keys[0]] == 0
+        with raises(KeyError):
+            rd[pX]
+        with raises(KeyError):
+            rd[p1]
+
+    def test_keys(self, keys_and_dict):
+        keys, rd = keys_and_dict
+        for idx, key in enumerate(keys):
+            assert key == rd.keys()[idx]
+        assert keys[0].name in rd.keys()
+    
+    def test_repr(self, keys_and_dict):
+        _, rd = keys_and_dict
+        assert repr(rd)
